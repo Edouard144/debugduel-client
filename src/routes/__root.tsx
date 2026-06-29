00c032a7ca8a0c90159bb043d7457d9e2dba88b3
Edouard <edouardtuyubahe@gmail.com>
2026-06-29 05:08:50 +0200

    Finalising the UI

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,6 +10,7 @@ import {
 import { useEffect, type ReactNode } from "react";
 
 import appCss from "../styles.css?url";
+import faviconUrl from "../lib/assets/favicon.ico?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 
 function NotFoundComponent() {
@@ -63,6 +64,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "twitter:card", content: "summary_large_image" },
     ],
     links: [
+      { rel: "icon", type: "image/x-icon", href: faviconUrl },
+      { rel: "shortcut icon", href: faviconUrl },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
